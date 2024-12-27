2f735fbb3c40cec0bc8c5a6d0d95b7fefe580830
Ezzz-lui <ogn.lui@gmail.com>
2024-12-27 17:47:00 -0600

    first commit

---
+++ arch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clonar el frontend
+if [ ! -d "frontend" ]; then
+  git clone https://github.com/Ezzz-Lui/PuntoDeVenta-POS.git frontend
+else
+  echo "El repositorio frontend ya existe. Actualizando..."
+  cd frontend && git pull && cd ..
+fi
+
+# Clonar el backend
+if [ ! -d "backend" ]; then
+  git clone https://github.com/Ezzz-Lui/PuntoDeVenta-POS-Backend.git backend
+else
+  echo "El repositorio backend ya existe. Actualizando..."
+  cd backend && git pull && cd ..
+fi
+
+# Levantar los contenedores
+docker-compose up -d
